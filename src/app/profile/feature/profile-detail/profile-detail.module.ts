67510c0819804ba372112edcbbd4a44798ba0e08
SimonWuensch-It-Studio-Rech <simon@it-studio-rech.io>
2023-06-20 17:29:36 +0200

    Pipe "phoneNumber" added.

--- src/app/profile/feature/profile-detail/profile-detail.module.ts
+++ src/app/profile/feature/profile-detail/profile-detail.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ProfileDetailRoutingModule } from './profile-detail-routing.module';
-import { ProfileDetailComponent } from './profile-detail.component';
+import {ProfileDetailRoutingModule} from './profile-detail-routing.module';
+import {ProfileDetailComponent} from './profile-detail.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {MatButtonModule} from "@angular/material/button";
+import {PhoneNumberPipe} from "../../ui/phone-number.pipe";
 
 
 @NgModule({
@@ -19,7 +20,9 @@ import {MatButtonModule} from "@angular/material/button";
     ReactiveFormsModule,
     MatInputModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    PhoneNumberPipe
   ]
 })
-export class ProfileDetailModule { }
+export class ProfileDetailModule {
+}
