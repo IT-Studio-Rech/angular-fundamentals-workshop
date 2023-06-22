6f056042609b09fb2856253344aa12f0210849b8
Jonas Rech <info@jonasrech.io>
2023-06-22 11:55:52 +0200

    add pipe

--- src/app/profile/feature/profile-detail/profile-detail.module.ts
+++ src/app/profile/feature/profile-detail/profile-detail.module.ts
@@ -1,19 +1,16 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {ProfileDetailRoutingModule} from './profile-detail-routing.module';
-import {ProfileDetailComponent} from './profile-detail.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import {MatInputModule} from "@angular/material/input";
-import {MatSelectModule} from "@angular/material/select";
-import {MatButtonModule} from "@angular/material/button";
-import {PhoneNumberPipe} from "../../ui/phone-number.pipe";
-
+import { ProfileDetailRoutingModule } from './profile-detail-routing.module';
+import { ProfileDetailComponent } from './profile-detail.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { PhoneNumberPipe } from '../../ui/pipes/phone-number.pipe';
 
 @NgModule({
-  declarations: [
-    ProfileDetailComponent
-  ],
+  declarations: [ProfileDetailComponent],
   imports: [
     CommonModule,
     ProfileDetailRoutingModule,
@@ -21,8 +18,7 @@ import {PhoneNumberPipe} from "../../ui/phone-number.pipe";
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
-    PhoneNumberPipe
-  ]
+    PhoneNumberPipe,
+  ],
 })
-export class ProfileDetailModule {
-}
+export class ProfileDetailModule {}
