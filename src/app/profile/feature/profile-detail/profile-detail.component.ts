fee824d69234e83432977ca6ac75512076e17fa4
Jonas Rech <info@jonasrech.io>
2023-06-22 16:02:00 +0200

    Adjust ProfileDetailComponent

--- src/app/profile/feature/profile-detail/profile-detail.component.ts
+++ src/app/profile/feature/profile-detail/profile-detail.component.ts
@@ -1,12 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {ProfileDetailsService} from "../../data-access/profile-details.service";
-import {firstValueFrom, tap} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ProfileDetailsService } from '../../data-access/profile-details.service';
 
 @Component({
   selector: 'app-profile-detail',
   templateUrl: './profile-detail.component.html',
-  styleUrls: ['./profile-detail.component.scss']
+  styleUrls: ['./profile-detail.component.scss'],
 })
 export class ProfileDetailComponent implements OnInit {
   firstName: string = '';
@@ -18,20 +17,19 @@ export class ProfileDetailComponent implements OnInit {
 
   countries = ['Deutschland', 'USA', 'Frankreich', 'UK', 'Italien', 'Spanien'];
 
-  constructor(private _profileDetailsService: ProfileDetailsService) {
-  }
+  constructor(private _profileDetailsService: ProfileDetailsService) {}
 
   ngOnInit() {
-    firstValueFrom(this._profileDetailsService.getProfileDetails().pipe(
-      tap(details => {
-        this.firstName = details.firstName;
-        this.lastName = details.lastName;
-        this.street = details.street;
-        this.houseNumber = details.houseNumber;
-        this.country = details.country;
-        this.phoneNumber = details.phoneNumber;
-      })
-    )).then(details => console.log('Profile details loaded. Details: ' + JSON.stringify(details)));
+    this._profileDetailsService.getProfileDetails().subscribe((details) => {
+      this.firstName = details.firstName;
+      this.lastName = details.lastName;
+      this.street = details.street;
+      this.houseNumber = details.houseNumber;
+      this.country = details.country;
+      this.phoneNumber = details.phoneNumber;
+
+      console.log(`profile details loaded ${JSON.stringify(details)}`);
+    });
   }
 
   saveProfile(form: NgForm) {
@@ -42,11 +40,14 @@ export class ProfileDetailComponent implements OnInit {
         street: this.street,
         houseNumber: this.houseNumber,
         country: this.country,
-        phoneNumber: this.phoneNumber
+        phoneNumber: this.phoneNumber,
       };
 
-      firstValueFrom(this._profileDetailsService.saveProfileDetails(profileDetails))
-        .then((result) => console.log('Save profile details - Result:', result));
+      this._profileDetailsService
+        .saveProfileDetails(profileDetails)
+        .subscribe((result) =>
+          console.log(`profile details saved ${JSON.stringify(result)}`)
+        );
     }
   }
 }
