c22f7195e5dc85152bd1cc69d532a07f6c8d82c6
SimonWuensch-It-Studio-Rech <simon@it-studio-rech.io>
2023-06-26 16:54:17 +0200

    Preperation: SERVICE PRAXIS

--- src/app/profile/feature/profile-detail/profile-detail.component.ts
+++ src/app/profile/feature/profile-detail/profile-detail.component.ts
@@ -1,13 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {ProfileDetailsService} from '../../data-access/profile-details.service';
 
 @Component({
   selector: 'app-profile-detail',
   templateUrl: './profile-detail.component.html',
   styleUrls: ['./profile-detail.component.scss'],
 })
-export class ProfileDetailComponent implements OnInit {
+export class ProfileDetailComponent {
   firstName: string = '';
   lastName: string = '';
   street: string = '';
@@ -17,20 +16,7 @@ export class ProfileDetailComponent implements OnInit {
 
   countries = ['Deutschland', 'USA', 'Frankreich', 'UK', 'Italien', 'Spanien'];
 
-  constructor(private _profileDetailsService: ProfileDetailsService) {
-  }
-
-  ngOnInit() {
-    this._profileDetailsService.getProfileDetails$().subscribe((details) => {
-      this.firstName = details.firstName;
-      this.lastName = details.lastName;
-      this.street = details.street;
-      this.houseNumber = details.houseNumber;
-      this.country = details.country;
-      this.phoneNumber = details.phoneNumber;
-
-      console.log(`profile details loaded ${JSON.stringify(details)}`);
-    });
+  constructor() {
   }
 
   saveProfile(form: NgForm) {
@@ -44,11 +30,7 @@ export class ProfileDetailComponent implements OnInit {
         phoneNumber: this.phoneNumber,
       };
 
-      this._profileDetailsService
-        .saveProfileDetails$(profileDetails)
-        .subscribe((result) =>
-          console.log(`profile details saved ${JSON.stringify(result)}`)
-        );
+      console.log(`profile details saved ${JSON.stringify(profileDetails)}`);
     }
   }
 }
