5e9af6780947d4dc2eba429fd6a2356d4a8aa8b6
SimonWuensch-It-Studio-Rech <simon@it-studio-rech.io>
2023-06-26 17:34:01 +0200

    Preperation: FOLDER STRUCTURE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,19 @@
 import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HeadlineComponent} from './shared/ui/component/headline/headline.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {CommonModule} from '@angular/common';
-import {MockProductInterceptor} from './products/data-access/mock/mock-products.interceptor';
-import {MockShoppingCartInterceptor} from './shopping-cart/data-access/mock/mock-shopping-cart.interceptor';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
-    CommonModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HeadlineComponent,
-    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: MockProductInterceptor,
-      multi: true,
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: MockShoppingCartInterceptor,
-      multi: true,
-    },
-  ],
-  bootstrap: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
